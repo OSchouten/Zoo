81b7b7719a519140e3ab45d2253b32262fb260f8
Olivier <olivier.schouten@hva.nl>
2023-12-04 13:56:59 +0100

    Extend interface in Tiger, Added process and perform command methods in the Zoo class.

--- src/com/ing/zoo/Tiger.java
+++ src/com/ing/zoo/Tiger.java
@@ -2,30 +2,24 @@ package com.ing.zoo;
 
 import java.util.Random;
 
-public class Tiger extends Animal implements Carnivore{
+public class Tiger extends Animal implements Carnivore, PerformTrick {
     public String trick;
 
-    public Tiger()
-    {
+    public Tiger() {
         super("rraaarww");
     }
 
-    public void eatMeat()
-    {
+    public void eatMeat() {
         eatText = "nomnomnom oink wubalubadubdub";
         System.out.println(eatText);
     }
 
-    public void performTrick()
-    {
+    public void performTrick() {
         Random random = new Random();
         int rnd = random.nextInt(2);
-        if(rnd == 0)
-        {
+        if (rnd == 0) {
             trick = "jumps in tree";
-        }
-        else
-        {
+        } else {
             trick = "scratches ears";
         }
         System.out.println(trick);
--- src/com/ing/zoo/Zoo.java
+++ src/com/ing/zoo/Zoo.java
@@ -1,15 +1,90 @@
 package com.ing.zoo;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Zoo {
-    public static void main(String[] args)
-    {
-        String[] commands = new String[4];
-        commands[0] = "hello";
-        commands[1] = "give leaves";
-        commands[2] = "give meat";
-        commands[3] = "perform trick";
+    private Map<String, Animal> animals;
+
+    public Zoo() {
+        this.animals = new HashMap<>();
+    }
+
+    public void addAnimal(Animal animal) {
+        animals.put(animal.name.toLowerCase(), animal);
+    }
+
+    public void processCommand(String input) {
+        String[] splitCommand = input.split(",");
+        if (splitCommand.length == 1) {
+            performCommand(splitCommand[0], null);
+        } else if (splitCommand.length == 2) {
+            String command = splitCommand[0];
+            String animalName = splitCommand[1].toLowerCase().trim();
+
+            Animal animal = animals.get(animalName);
+            if (animal != null) {
+                performCommand(command, animal);
+            } else {
+                System.out.println("Unknown animal: " + animalName);
+            }
+        } else {
+            System.out.println("Invalid command: " + input);
+        }
+    }
+
+    private void performCommand(String command, Animal specificAnimal) {
+        switch (command) {
+            case "hello":
+                if (specificAnimal != null) {
+                    specificAnimal.sayHello();
+                } else {
+                    for (Animal animal : animals.values()) {
+                        animal.sayHello();
+                    }
+                }
+                break;
+
+            case "give leaves":
+                if (specificAnimal instanceof Herbivore) {
+                    ((Herbivore) specificAnimal).eatLeaves();
+                } else {
+                    System.out.println(specificAnimal.name + " won't eat meat.");
+                }
+                break;
+
+            case "give meat":
+                if (specificAnimal instanceof Carnivore) {
+                    ((Carnivore) specificAnimal).eatMeat();
+                } else {
+                    System.out.println(specificAnimal.name + " won't eat leaves.");
+                }
+                break;
+
+            case "perform trick":
+                if (specificAnimal instanceof PerformTrick) {
+                    ((PerformTrick) specificAnimal).performTrick();
+                } else if (specificAnimal == null) {
+                    for (Animal animal : animals.values()) {
+                        if (animal instanceof PerformTrick) {
+                            ((PerformTrick) animal).performTrick();
+                        }
+                    }
+                } else {
+                    System.out.println(specificAnimal.name + " won't't perform any tricks.");
+                }
+                break;
+
+            default:
+                System.out.println("Unknown command: " + command);
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
 
         Lion henk = new Lion();
         henk.name = "henk";
@@ -22,17 +97,27 @@ public class Zoo {
         Zebra marty = new Zebra();
         marty.name = "marty";
 
+        zoo.addAnimal(henk);
+        zoo.addAnimal(elsa);
+        zoo.addAnimal(dora);
+        zoo.addAnimal(wally);
+        zoo.addAnimal(marty);
+
+        StringBuilder commandsBuilder = new StringBuilder();
+        commandsBuilder.append("The following commands are available:\n");
+        commandsBuilder.append("hello\n");
+        commandsBuilder.append("give leaves\n");
+        commandsBuilder.append("give meat\n");
+        commandsBuilder.append("perform trick\n");
+        commandsBuilder.append("Use the format: Command, AnimalName");
+        String commandsString = commandsBuilder.toString();
+        System.out.println(commandsString);
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Voer uw command in: ");
+        System.out.print("Enter your command: ");
 
         String input = scanner.nextLine();
-        if(input.equals(commands[0] + " henk"))
-        {
-            henk.sayHello();
-        }
-        else
-        {
-            System.out.println("Unknown command: " + input);
-        }
+
+        zoo.processCommand(input);
     }
 }
